07a80a25f5abfde9e753fa7733b70c2d9e40a3e9
Aleksey! <Alekmog@yandex.ru>
2021-03-02 11:07:23 +0300

    Added file check for writing AND changed the check on file open

--- Service/FileStorageService.php
+++ Service/FileStorageService.php
@@ -2,25 +2,45 @@
 
 namespace MyApp\Service;
 
-use Error;
-use Exception;
+use RuntimeException;
 
 class FileStorageService implements StorageServiceInterface
-{   
+{
     public function storeComments(array $comments, string $file): void
-    {  
+    {
+        if (file_exists($file)) {
+            try {
+                if (!is_writable($file)) {
+                    throw new RuntimeException('Файл не доступен для записи');
+                }
+            } catch (RuntimeException $e) {
+                echo '<br>';
+                echo 'Ошибка: ' . $e->getMessage();
+                echo '<br>';
+                echo 'Файл: ' . $e->getFile();
+                echo '<br>';
+                echo 'Строка: ' . $e->getLine();
+                exit;
+            }
+        }       
+
         $handle = fopen($file, 'w');
 
-        if ($handle === false) {
-            throw new Exception('Ошибка открытия файла');
+        try {
+            if ($handle === false) {
+                throw new RuntimeException('Неизвестная ошибка открытия файла');
+            }
+        } catch (RuntimeException $e) {
+            echo $e->getMessage();
+            exit;
         }
-        
-        foreach ($comments as $key => $value) {            
-            fwrite($handle, 'Пост № ' . $key + 1 . PHP_EOL); 
-            fwrite($handle, 'Автор: ' . $value->getAuthor() . PHP_EOL); 
+
+        foreach ($comments as $key => $value) {
+            fwrite($handle, 'Пост № ' . $key + 1 . PHP_EOL);
+            fwrite($handle, 'Автор: ' . $value->getAuthor() . PHP_EOL);
             fwrite($handle, 'Сообшение: ' . $value->getText() . PHP_EOL);
         }
 
-        fclose($handle);        
+        fclose($handle);
     }
 }
